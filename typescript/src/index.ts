79f179072ccdd6166e8926c904472653651a8fc5
Joshua Lee <josh.lee134@hotmail.com>
2023-06-16 14:49:31 -0700

    do some problems

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,11 @@
-const str = "Hello, world!";
+(
+    async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then( response => response.json());
+        let num: string | number = "2";
+        
+        const nums = [ 1 , "2", 3];
+        nums[0]
+    }
+)();
 
