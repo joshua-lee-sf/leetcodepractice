147c74ad5bc5daf84b76bec1c6cb58903f21ba10
Joshua Lee <josh.lee134@hotmail.com>
2023-06-16 14:49:56 -0700

    add some problems

--- practiceproblems.js
+++ practiceproblems.js
@@ -25,3 +25,16 @@ const searchInsert = function(nums, target) {
 
     return ans;
 };
+
+var removeDuplicates = function(nums) {
+    let count = 0
+
+    for(let i = 1; i < nums.length; i++){
+        if (nums[count] !== nums[i]){
+            count++;
+            nums[count] = nums[i]
+        }
+    }
+
+    return count + 1
+};
