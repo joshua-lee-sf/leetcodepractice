60aa954cc153f8f2b988e8aabc8ed3e6f39e0100
Joshua Lee <josh.lee134@hotmail.com>
2023-06-22 11:48:38 -0700

    add number 1700

--- practiceproblems.js
+++ practiceproblems.js
@@ -170,3 +170,21 @@ MyLinkedList.prototype.deleteAtIndex = function(index) {
         }   
     }
 };
+
+// 1700. students unable to eat lunch
+var countStudents = function(students, sandwiches) {
+    let count = 0;
+
+    while (count < students.length && students.length > 0){
+        if (students[0] === sandwiches[0]){
+            count = 0;
+            students.shift();
+            sandwiches.shift();
+        } else {
+            count++
+            students.push(students.shift());
+        }
+    }
+
+    return students.length;
+};
