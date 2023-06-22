a5a2819805585c6ff312053acbbd69d8c00c09e8
Joshua Lee <josh.lee134@hotmail.com>
2023-06-22 12:32:06 -0700

    add number 225

--- practiceproblems.js
+++ practiceproblems.js
@@ -188,3 +188,40 @@ var countStudents = function(students, sandwiches) {
 
     return students.length;
 };
+
+// 225.
+var MyStack = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    for (let i = 0; i < this.queue.length - 1; i++){
+        this.queue.push(this.queue.shift());
+    }
+    return this.queue.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[this.queue.length - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0
+};
