488030c23624519b830c469c452619f7d73cf328
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-06-26 18:42:13 -0700

    Time: 1167 ms (31.45%) | Memory: 89.9 MB (16.29%) - LeetSync

--- sort-an-array/sort-an-array.js
+++ sort-an-array/sort-an-array.js
@@ -2,16 +2,38 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-    for (let i = 1; i < nums.length; i++){
-        let j = i - 1;
-        while (j >= 0 && nums[j + 1] < nums[j]){
-            let temp = nums[j + 1];
-            nums[j + 1] = nums[j];
-            nums[j] = temp;
-            j -= 1;
+var sortArray = function(nums, func) {
+    if (nums.length <= 1) return nums;
+
+    if (!func){
+        func = (left, right) => {
+            return left < right ? -1 : left > right ? 1 : 0;
         }
     }
 
-    return nums
+    const midpoint = Math.floor(nums.length / 2);
+    const left = sortArray(nums.slice(0, midpoint), func);
+    const right = sortArray(nums.slice(midpoint), func);
+    return merger(left, right, func);
 };
+
+const merger = (left, right, func) => {
+    let merged = []
+    
+    while (left.length && right.length){
+        switch(func(left[0], right[0])){
+            case -1:
+                merged.push(left.shift());
+                break;
+            case 0:
+                merged.push(left.shift());
+                break;
+            case 1:
+                merged.push(right.shift());
+                break;
+        }
+    }
+
+    merged = merged.concat(left, right)
+    return merged;
+}
