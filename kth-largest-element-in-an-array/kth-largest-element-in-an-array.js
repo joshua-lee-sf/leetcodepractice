866d579261d88e9bed7e83f55143d03d89df9bc3
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-06-29 18:48:18 -0700

    Time: 138 ms (71.55%) | Memory: 49.1 MB (89.03%) - LeetSync

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,15 +4,29 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    // const singleNums = [];
+    k = nums.length - k;
 
-    // nums.forEach(num => {
-    //     if (!singleNums.includes(num)) singleNums.push(num);
-    // })
+    const quickSelect = (l, r) => {
+        let pointer = l;
+        let pivot = nums[r];
 
-    const sorted = nums.sort( function(a,b) {
-        return a-b
-    });
-    const length = sorted.length;
-    return sorted[length - k];
+        for (let i = l; i < r; i++){
+            if (nums[i] <= pivot) {
+                let temp = nums[pointer];
+                nums[pointer] = nums[i];
+                nums[i] = temp;
+                pointer++; 
+            }
+        }
+
+        let temp = nums[pointer];
+        nums[pointer] = nums[r];
+        nums[r] = temp;
+
+        if (pointer > k) return quickSelect(l, pointer - 1);
+        else if (pointer < k) return quickSelect(pointer + 1, r);
+        else return nums[pointer];
+    }
+
+    return quickSelect(0, nums.length - 1)
 };
