be6a2b9619cb8c7a864e994c1597fc90868453fd
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-07-03 21:30:36 -0700

    Time: 61 ms (47.41%) | Memory: 42.4 MB (29.03%) - LeetSync

--- search-a-2d-matrix/search-a-2d-matrix.js
+++ search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,69 +4,36 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    // ROW = matrix.length;
-    // COLUMN = matrix[0].length;
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let top = 0; bottom = rows - 1;
 
-    // let top = 0; bottom = ROW - 1;
-    // while(top <= bottom){
-    //     let row = Math.floor(top + bottom / 2);
-    //     if (target > matrix[row][COLUMN - 1]){
-    //         top = row + 1;
-    //     } else if (target < matrix[row][0]){
-    //         bottom = row - 1;
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // if (!(top <= bottom)) return false;
-
-    // let row = Math.floor((top + bottom) / 2);
-    // let l = 0, r = COLUMN - 1;
-    // while (l <= r){
-    //     let m = Math.floor((l + r) / 2);
-    //     if (target > matrix[row][m]){
-    //         l = m + 1;
-    //     } else if (target < matrix[row][m]){
-    //         r = m - 1;
-    //     } else if (target === matrix[row][m]) {
-    //         return true;
-    //     }
-    // }
-
-    // return false;
-
-    let [rows, cols] = [matrix.length, matrix[0].length];
-    let [top, bot] = [0, rows-1];
-    
-    while(top <= bot){
-        let row = Math.floor((top +  bot) / 2);
-        if(target > matrix[row][cols-1]) {
+    while(top <= bottom){
+        let row = Math.floor((top + bottom) / 2);
+        if (target > matrix[row][cols - 1]){
             top = row + 1;
-        } else if(target < matrix[row][0]) {
-            bot = row - 1; 
+        } else if (target < matrix[row][0]){
+            bottom = row - 1;
         } else {
             break;
         }
     }
-    
-    if(!(top <= bot)) {
-        return false;
-    }
-    
-    let row = Math.floor((top + bot) / 2);
-    let [l, r] = [0, cols-1];
-    while(l<=r){
-        let m = Math.floor((l + r) /2);
-        if(target > matrix[row][m]) {
-            l = m +1;
-        } else if(target < matrix[row][m]) {
+
+    if (!(top <= bottom)) return false;
+
+    let row = Math.floor((top + bottom) / 2);
+    let l = 0, r = cols - 1;
+    while (l <= r){
+        let m = Math.floor((l + r) / 2);
+        if (target > matrix[row][m]){
+            l = m + 1;
+        } else if (target < matrix[row][m]){
             r = m - 1;
-        } else if(target == matrix[row][m]) {
+        } else if (target === matrix[row][m]) {
             return true;
         }
     }
-    return false;
 
+    return false;
 
 };
