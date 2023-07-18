4a2d480bd608c57e14822b1057d8e37f506fcfb7
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-07-17 20:28:26 -0700

    Time: 57 ms (99.92%) | Memory: 45.1 MB (85.07%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -11,20 +11,21 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    const queue = [root];
-    const tree = [];
+    let queue = [root];
+    let tree = []
 
-    while (queue[0]){
+    while(queue.length > 0){
         let subTree = [];
         let length = queue.length;
         for (let i = 0; i < length; i++){
             let curr = queue.shift();
-            subTree.push(curr.val);
-            if (curr.left) queue.push(curr.left);
-            if (curr.right) queue.push(curr.right);
+            if (curr){
+                subTree.push(curr.val);
+                if (curr.left) queue.push(curr.left);
+                if (curr.right) queue.push(curr.right);
+            }
         }
-        tree.push(subTree);
+        if (subTree.length > 0) tree.push(subTree);
     }
-
-    return tree;
+    return tree
 };
