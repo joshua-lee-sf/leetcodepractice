17923ca2eddf1913a1e0855df6b374a8408cfa00
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-07-29 23:11:31 -0700

    Time: 57 ms (66.58%) | Memory: 42.2 MB (27.06%) - LeetSync

---
+++ leetcodepractice/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ // haystack = mississippi
+ // needle = issip
+var strStr = function(haystack, needle) {
+    let k = 0;
+
+    for (let i = 0; i < haystack.length; i++){
+        if (haystack[i]  === needle[k]){ // p === p / k = 5
+            k++
+            if (k === needle.length){
+                return i - (k - 1) // 8 - (5 - 1)
+            }
+        } else {
+            i = i - k
+            k = 0
+        }
+    }
+
+    return -1
+};
