b4c9663812a1712acb6a4c2297eb74631e45b6e0
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-07-29 17:03:56 -0700

    Time: 3006 ms (25.70%) | Memory: 69.2 MB (23.49%) - LeetSync

--- leetcodepractice/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
+++ leetcodepractice/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
@@ -1,45 +1,23 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k;
+    this.nums = nums;
+};
+
 /** 
- * https://leetcode.com/problems/kth-largest-element-in-a-stream/
- * Time O(N * (K * log(K))) | Space O(K)
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.nums.push(val);
+    return this.nums.sort((a,b) => b-a)[this.k-1]
+};
+
+/** 
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
- class KthLargest {
-    /**
-     * @param {number} k
-     * @param {number[]} nums
-     * @constructor
-     */
-    constructor(k, nums) {
-        this.k = k
-        this.minHeap = new MinPriorityQueue();
-
-        nums.forEach((num) => this.add(num))
-    }
-
-    /**
-     * @param {number} val
-     * @return {number}
-     */
-    add(val, { minHeap } = this) {
-        const isUnderCapacity = minHeap.size() < this.k;
-        if (isUnderCapacity) {
-            minHeap.enqueue(val);
-        
-            return this.top();
-        }
-
-        const isLarger = this.top() < val;
-        if (isLarger) {
-            minHeap.dequeue()
-            minHeap.enqueue(val);
-        }
-        
-        return this.top();
-    }
-
-    top ({ minHeap } = this) {
-        return minHeap.front()?.element || 0
-    }
-}
