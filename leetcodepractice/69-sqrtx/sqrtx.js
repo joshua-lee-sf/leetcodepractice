d5ec35b6e218cd98d0e35ff87a5fcccb09af07a2
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-08-09 16:13:40 -0700

    Time: 51 ms (98.61%) | Memory: 44.1 MB (26.88%) - LeetSync

--- leetcodepractice/69-sqrtx/sqrtx.js
+++ leetcodepractice/69-sqrtx/sqrtx.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(x ** (1 / 2));
+    if (x === 0 || x === 1) {
+        return x;
+    }
+
+    let left = 1;
+    let right = x;
+    let result = 0;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        // Check if mid*mid is less than or equal to x
+        if (mid * mid <= x) {
+            result = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
 };
