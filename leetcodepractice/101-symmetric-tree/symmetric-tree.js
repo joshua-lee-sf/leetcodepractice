45ca8575c196f1c85c9c4baf535cc3ecc5d5144b
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-08-17 11:56:25 -0700

    Time: 65 ms (57.21%) | Memory: 44.8 MB (20.39%) - LeetSync

---
+++ leetcodepractice/101-symmetric-tree/symmetric-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+
+    const dfs = (left, right) => {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        return left.val === right.val && left.val === right.val && dfs(left.right, right.left) && dfs(left.left, right.right);
+    }
+
+    return dfs(root.left, root.right);
+};
