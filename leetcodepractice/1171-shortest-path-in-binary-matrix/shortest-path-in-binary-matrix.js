863b906c27b9111bec74a7dba2f0168e2e548ba1
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-09-28 09:47:07 -0700

    Time: 195 ms (30.90%) | Memory: 61.8 MB (26.51%) - LeetSync

--- leetcodepractice/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
+++ leetcodepractice/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
@@ -3,32 +3,36 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function(grid) {
-    const N = grid.length
-    const visited = new Set();
+    const N = grid.length;
     const queue = [];
+    const visited = new Set();
+    const directions = [[0,1], [0,-1], [1,0], [-1,0],
+                        [1,1], [1,-1], [-1,1], [-1,-1]]
     queue.push([0,0,1]);
     visited.add('0,0');
-    const directions = [[0,1], [0, -1], [1,0], [-1,0], 
-                        [1,1], [1,-1], [-1,1], [-1, -1]]
 
-    while (queue.length > 0) {
-        let [r, c, length] = queue.shift();
+    while (queue[0]) {
+        let [r,c,length] = queue.shift();
 
-        if (Math.min(r,c) < 0 || 
-        Math.max(r,c) >= N || 
-        grid[r][c] ) {
+        // check to see if r,c is within the bounds and not 0
+        if (Math.min(r,c) < 0 || Math.max(r,c) >= N || grid[r][c]){
             continue;
         }
+        // check to see if we reached the end 
         if (r === N - 1 && c === N - 1) return length;
 
-        for (const [dr,dc] of directions){
+        // loop through possible directions
+        for (let [dr, dc] of directions) {
             const newRow = r + dr;
             const newCol = c + dc;
-            if (!visited.has(`${newRow}, ${newCol}`)){
+
+            if (!visited.has(`${newRow}, ${newCol}`)) {
                 queue.push([newRow, newCol, length + 1]);
-                visited.add(`${newRow}, ${newCol}`)
+                visited.add(`${newRow}, ${newCol}`);
             }
         }
     }
+
+    // if the program loops through the entire iteration and couldnt return the length, it's not possible
     return -1
 };
