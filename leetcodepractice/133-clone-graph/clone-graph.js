dc2c54bd8c03936c79e085a5a2ff2d96b61cebfd
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-10-30 18:43:51 -0700

    Time: 60 ms (24.42%) | Memory: 44.1 MB (19.58%) - LeetSync

---
+++ leetcodepractice/133-clone-graph/clone-graph.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node, seen = new Map()) {
+    const isBaseCase = node === null;
+    if (isBaseCase) return null;
+
+    if (seen.has(node)) return seen.get(node);
+
+    return dfs(node, seen);
+};
+
+const dfs = (node, seen) => {
+    const clone = new Node(node.val);
+
+    seen.set(node, clone);
+
+    for (const neighbor of node.neighbors){
+        const cloneNeighbor = cloneGraph(neighbor, seen);
+
+        clone.neighbors.push(cloneNeighbor);
+    }
+
+    return clone;
+}
