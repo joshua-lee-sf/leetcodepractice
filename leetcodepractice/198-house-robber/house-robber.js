c03b613590fbe352ae66de8df1d23d6fabe67a43
Joshua Lee <110413419+joshua-lee-sf@users.noreply.github.com>
2023-11-01 12:53:19 -0700

    Time: 47 ms (79.86%) | Memory: 42.4 MB (10.52%) - LeetSync

--- leetcodepractice/198-house-robber/house-robber.js
+++ leetcodepractice/198-house-robber/house-robber.js
@@ -2,14 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    let [rob1, rob2] = [0,0];
+var rob = function(nums, i=0, memo = initMemo(nums)) {
+    const isBaseCase = nums.length <= i;
+    if (isBaseCase) return 0;
 
-    for (let num of nums) {
-        let temp = Math.max(num + rob1, rob2);
-        rob1 = rob2;
-        rob2 = temp;
-    }
+    const hasSeen = 0 <= memo[i];
+    if (hasSeen) return memo[i];
 
-    return rob2;
+    const [next, nextNext] = [(i + 1), (i + 2)];
+    const right = nums[i];
+    const mid = rob(nums, next, memo);
+    const left = rob(nums, nextNext, memo);
+    const house = left + right;
+
+    memo[i] = Math.max(mid, house);
+
+    return memo[i];
 };
+
+const initMemo = (nums) => Array(nums.length + 1).fill(-1)
